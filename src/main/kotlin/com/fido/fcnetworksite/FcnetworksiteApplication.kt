b5f932d33f991f4fb718074a239eada2a9830b87
wangxianfei <i_wangxianfei@cvte.com>
2018-10-11 17:36:52 +0800

    init project

---
+++ src/main/kotlin/com/fido/fcnetworksite/FcnetworksiteApplication.kt
@@ -0,0 +1,16 @@
+package com.fido.fcnetworksite
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+/**
+ * @author: fido
+ * @desription:
+ * @date: Created in 17:11 2018/10/11
+ */
+@SpringBootApplication
+class FcnetworksiteApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(FcnetworksiteApplication::class.java, *args)
+}
