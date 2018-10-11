414c9c5fc8af09d594ed95de7b4c087a890415de
wangxianfei <i_wangxianfei@cvte.com>
2018-10-11 23:15:10 +0800

    init project

---
+++ src/main/kotlin/com/fido/fcnetworksite/entity/User.kt
@@ -0,0 +1,13 @@
+package com.fido.fcnetworksite.entity
+
+/**
+ * @author: fido
+ * @date: 2018/10/11 23:02
+ * @description:用户表
+ */
+
+data class User(
+        val userId: Integer,
+        val loginName: String,
+        val nickName:String
+)
