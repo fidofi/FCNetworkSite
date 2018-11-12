7718d3750a70b534717641a29defc78a6ff3b984
wangxianfei <i_wangxianfei@cvte.com>
2018-11-12 17:53:39 +0800

    增加redis模板

---
+++ src/main/kotlin/com/fido/fcnetworksite/controller/TimeLineController.kt
@@ -0,0 +1,14 @@
+package com.fido.fcnetworksite.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * @author: wangxianfei
+ * @desription:
+ * @date: Created in 8:59 2018/11/12
+ */
+@RestController
+@RequestMapping
+class TimeLineController {
+}
